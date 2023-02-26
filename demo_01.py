c056d62740c9d28c62a8cf905b7a5b653614b5e5
Emrys <1260078333@qq.com>
2023-02-26 13:17:11 +0800

    2023/2/26 第一次提交！

--- demo_01.py
+++ demo_01.py
@@ -1,2 +1,17 @@
-print('I love qxy!')
-print('Emrys')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+wd = webdriver.Chrome()
+wd.implicitly_wait(10)
+
+wd.get('https://cdn2.byhy.net/files/selenium/sample1.html')
+
+sleep(1)
+
+element = wd.find_element(By.CSS_SELECTOR, '#searchtext')
+element.send_keys('您好')
+
+sleep(1)
+
+wd.quit()
